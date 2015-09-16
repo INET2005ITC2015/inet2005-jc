df8cf7b7bdc91f2fa894a46281e6b9eac4fa7003
jrcooke2 <jrcooke2@gmail.com>
2015-09-16 10:55:42 -0700

    Started Lab 1 - Part A - Step 6 (Force closing app, debugger not working, still not done)

--- Lab1/KiloPoundConverter.php
+++ Lab1/KiloPoundConverter.php
@@ -16,15 +16,15 @@
 		        
 		        $poundsResult = $kilosResult * 2.2;
 		        
-		        echo "<p>" + $kilosResult + " kilos equals " + $poundResult + " pounds.</p>";
+		        echo "<p>  $kilosResult "."  kilos equals  "." $poundsResult "."  pounds.</p>";
 		    }
-		    elseif (!isset($_POST['ConverttoKilos']))
+		    elseif (isset($_POST['ConverttoKilos']))
 		    {
-		    	$poundsResult = $_POST['pound'];
+		    	$poundsResult = $_POST['pounds'];
 		    
 		    	$kilosResult = $poundsResult / 2.2;
 		    	
-		    	echo "<p>" + $poundsResult + " pounds equals " + $kilosResult + " kilos.</p>";
+		    	echo "<p> $poundsResult "."  pounds equals  "." $kilosResult "." kilos.</p>";
 		    }
 		    else
 		    {
