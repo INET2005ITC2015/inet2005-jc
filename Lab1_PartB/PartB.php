ac2c55b7078cbb0418939992268b1b24435fd499
jrcooke2 <jrcooke2@gmail.com>
2015-09-17 07:33:14 -0700

    Lab 1 - Part B, Finished Product

--- Lab1_PartB/PartB.php
+++ Lab1_PartB/PartB.php
@@ -13,5 +13,34 @@ echo "Hey $yourName"; ?>
  <p><?php echo (1024 / 128) -7; ?></p>
  <p><?php echo (32 * 12) + 83; ?></p>
  <p><?php echo (796 % 6); ?></p>
+ <p>
+     <?php
+        for ($x = 10;$x >0; $x--){
+            echo $x."...";
+        }
+    echo "Blast Off"; ?>
+ </p>
+ <p>
+     <?php
+        $colors = array ("Blue", "Red", "White", "Purple", "Cyan", "Green", "Magenta");
+        for ($x = 0; $x <= 7; $x++){
+            echo $colors[$x]. " ";
+        }
+    ?>
+ </p>
+ <p>
+     <?php
+     $colors = array ("Blue", "Red", "White", "Purple", "Cyan", "Green", "Magenta");
+     foreach ($colors as $value){
+         echo $value. " ";
+     }
+     ?>
+ </p>
+ <p>
+     <?php
+     $colors = array ("Blue", "Red", "White", "Purple", "Cyan", "Green", "Magenta");
+     echo $colors[0] . " " . $colors[1] . " " . $colors[2] . " " . $colors[3] . " " . $colors[4] . " " . $colors[5] . " " . $colors[6] . " " . $colors[7];
+     ?>
+ </p>
 </body>
 </html>
