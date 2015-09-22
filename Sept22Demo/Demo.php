419db38be04fd90fd98910bb36b303d4c12afed3
jrcooke2 <jrcooke2@gmail.com>
2015-09-22 04:45:09 -0700

    Starting Demo

--- Sept22Demo/Demo.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: inet2005
- * Date: 9/22/15
- * Time: 4:37 AM
- */
---
+++ Sept22Demo/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html>
+        <head lang="em">
+                <meta charset="UTF-8">
+                <title></title>
+        </head>
+        <body>
+            <!--Comment HTML -->
+            <?php
+            // enter code here
+            $firstName = "Justin";
+            $lastName = "Cooke";
+
+            echo "Hello from $firstName $lastName!";
+            ?>
+        </body>
+
+</html>
