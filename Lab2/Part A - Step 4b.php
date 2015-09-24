22af403c8ee49d49957df75494dc98030a4a3972
jrcooke2 <jrcooke2@gmail.com>
2015-09-24 09:22:26 -0700

    Started Lab 1 - Part A - Step 1 to 4 Complete

---
+++ Lab2/Part A - Step 4b.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+    <?php
+
+    echo "<h1>Current php version: ". phpversion();"</h1>";
+    echo "<h1>Current zend version: ". zend_version();"</h1>";
+    echo "<h1>Default mime type : ". ini_get("default_mimetype");"</h1>";
+
+
+    ?>
+</body>
+</html>
