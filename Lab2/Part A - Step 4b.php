bf4347bede5730db8d99b7c93db707790b6dcce2
jrcooke2 <jrcooke2@gmail.com>
2015-09-25 07:01:31 -0700

    Started Lab 1 - Part C up to Zodiac Complete

--- Lab2/Part A - Step 4b.php
+++ Lab2/Part A - Step 4b.php
@@ -6,12 +6,9 @@
 </head>
 <body>
     <?php
-
     echo "<h1>Current php version: ". phpversion();"</h1>";
     echo "<h1>Current zend version: ". zend_version();"</h1>";
     echo "<h1>Default mime type : ". ini_get("default_mimetype");"</h1>";
-
-
     ?>
 </body>
 </html>
--- Lab2/Part Cb.php
+++ Lab2/Part Cb.php
@@ -9,7 +9,7 @@
 <?php
 
 $name = $_GET["fName"]. " " . $_GET["lName"];
-echo "<p>Hello, $name</p>.";
+echo "<p>Hello, $name</p>";
 $month = $_GET["bMonth"];
 $day = $_GET["bDay"];
 $sign = " ";
@@ -20,33 +20,78 @@ switch ($month) {
     }else{
         $sign = "Aquarius";
     }
+        break;
     case "02": if($day <= 18){
         $sign = "Aquarius";
     }else{
         $sign = "Pisces";
     }
+        break;
     case "03": if($day <= 20){
         $sign = "Pisces";
     }else{
         $sign = "Aries";
     }
-    case "04": if($day <= 21){
+        break;
+    case "04": if($day <= 19){
         $sign = "Aries";
     }else{
         $sign = "Taurus";
     }
+        break;
     case "05": if($day <= 20){
         $sign = "Taurus";
     }else{
         $sign = "Gemini";
     }
-
-
+        break;
+    case "06": if($day <= 20){
+        $sign = "Gemini";
+    }else{
+        $sign = "Cancer";
+    }
+        break;
+    case "07": if($day <= 22){
+        $sign = "Cancer";
+    }else{
+        $sign = "Leo";
+    }
+        break;
+    case "08": if($day <= 22){
+        $sign = "Leo";
+    }else{
+        $sign = "Virgo";
+    }
+        break;
+    case "09": if($day <= 22){
+        $sign = "Virgo";
+    }else{
+        $sign = "Libra";
+    }
+        break;
+    case "10": if($day <= 22){
+        $sign = "Libra";
+    }else{
+        $sign = "Scorpio";
+    }
+        break;
+    case "11": if($day <= 21){
+        $sign = "Scorpio";
+    }else{
+        $sign = "Sagittarius";
+    }
+        break;
+    case "12": if($day <= 21){
+        $sign = "Sagittarius";
+    }else{
+        $sign = "Capricorn";
+    }
+        break;
 };
 
-
-
+echo "<p>Your zodiac sign is: $sign</p>";
 ?>
+<p><a href="Part Cb html.html">Back to Forum</a></p>
 
 </body>
 </html>
