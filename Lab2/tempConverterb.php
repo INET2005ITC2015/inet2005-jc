639e11613a1e9a9c2ce91e3d7ee58e0ba77219db
jrcooke2 <jrcooke2@gmail.com>
2015-09-25 05:08:59 -0700

    Started Lab 1 - Part B Complete

--- Lab2/tempConverterb.php
+++ Lab2/tempConverterb.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-  <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="styles.css">
-  <title></title>
+    <title></title>
 </head>
 <body>
 <table>
     <thead>
     <tr>
-        <th>Fahrenheit</th>
         <th>Celsius</th>
+        <th>Fahrenheit</th>
     </tr>
     </thead>
     <tbody>
     <?php
     for ($i =0; $i <= 100; $i++):
-        $celsius = ($i - 32)*(5/9);
+        $fahrenheit = ($i *(9/5)) + 32;
         ?>
         <tr>
             <td><?php echo $i; ?></td>
-            <td><?php echo round($celsius); ?></td>
+            <td><?php echo round($fahrenheit); ?></td>
         </tr>
     <?php
     endfor;
