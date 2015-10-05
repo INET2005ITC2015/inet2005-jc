cf43c1884074b91cf51cb50c26fa2e4a1049111d
jrcooke2 <jrcooke2@gmail.com>
2015-10-05 15:57:53 -0700

    Lab3B -  Working on Updating

--- Lab3B/index3.php
+++ Lab3B/index3.php
@@ -20,11 +20,11 @@ echo "<h1>$actorId</h1>";
 
 
 $resultUpdate = mysqli_query($db,"UPDATE actor SET first_name = '$fName', last_name = '$lName' WHERE actor_id= '$actorId';");
-if(!$resultDel)
+if(!$resultUpdate)
 {
     die('Could not Update records in the Sakila Database: ' . mysqli_error($db));
 }else{
-    echo "<p>Successful! You have deleted"." ".mysqli_affected_rows($db)." ". "row(s).</p>";
+    echo "<p>Successful! You have Updated"." ".mysqli_affected_rows($db)." ". "row(s).</p>";
 }
 
 closeDBConnection($db);
