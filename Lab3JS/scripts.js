7593c4bf9a9147fab68d949fcbec92fa49252e85
jrcooke2 <jrcooke2@gmail.com>
2015-10-05 18:03:53 -0700

    Started Form

--- Lab3JS/scripts.js
+++ Lab3JS/scripts.js
@@ -1,3 +1,15 @@
 /**
  * Created by inet2005 on 10/5/15.
  */
+"use strict";
+
+function focus(){
+
+    document.getElementById(document.activeElement).style.backgroundColor="Yellow"
+
+}
+
+function blur(){
+
+}
+
