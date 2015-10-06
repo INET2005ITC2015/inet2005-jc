30bf754d7bc260b311808fcc530457cdd59826c4
jrcooke2 <jrcooke2@gmail.com>
2015-10-06 06:24:30 -0700

    Forms Complete

--- Lab3JS/scripts.js
+++ Lab3JS/scripts.js
@@ -1,15 +1,58 @@
-/**
- * Created by inet2005 on 10/5/15.
- */
-"use strict";
+function checkForm(){
 
-function focus(){
+    if(document.forms["Form"].firstName.value.length ==0)
+    {
+        document.getElementById("firstName").style.borderColor = "red";
+        alert("Please fill in highlighted text field");
+        return false;
+    }
+    else if(document.forms["Form"].lastName.value.length ==0)
+    {
+        document.getElementById("lastName").style.borderColor = "red";
+        alert("Please fill in highlighted text field");
+        return false;
+    }
+    else if(document.forms["Form"].address1.length ==0)
+    {
+        document.getElementById("address1").style.borderColor = "red";
+        alert("Please fill in highlighted text field");
+        return false;
+    }
+    else if(document.forms["Form"].address2.length ==0)
+    {
+        document.getElementById("address2").style.borderColor = "red";
+        alert("Please fill in highlighted text field");
+        return false;
+    }
+    else if(document.forms["Form"].email.length ==0)
+    {
+        document.getElementById("email").style.borderColor = "red";
+        alert("Please fill in highlighted text field");
+        return false;
+    }
+    else if(document.Form.accept.checked == false)
+    {
+        document.getElementById("Warning").innerHTML = "Please check that you have accepted terms!";
+        document.getElementById("Warning").style.color = "red";
+        return false;
+    }
+}
+function change(fieldID){
 
-    document.getElementById(document.activeElement).style.backgroundColor="Yellow"
-
+    var formItem = document.getElementById(fieldID);
+    if(formItem != null){
+        formItem.style.backgroundColor = "Yellow";
+        formItem.style.fontStyle = "Italic";
+        formItem.parentNode.style.textDecoration = "underline"
+    }
 }
 
-function blur(){
-
+function changeBack(fieldID){
+    var formItem = document.getElementById(fieldID);
+    if(formItem != null){
+        formItem.style.backgroundColor = "White";
+        formItem.style.fontStyle = "normal";
+        formItem.parentNode.style.textDecoration = "none"
+    }
 }
 
