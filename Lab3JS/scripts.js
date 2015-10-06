e437663f616d23665c746e8d1af967a7115874a2
jrcooke2 <jrcooke2@gmail.com>
2015-10-06 08:39:07 -0700

    Forms Complete

--- Lab3JS/scripts.js
+++ Lab3JS/scripts.js
@@ -1,41 +1,38 @@
-function checkForm(){
+function checkForm() {
 
-    if(document.forms["Form"].firstName.value.length ==0)
-    {
+    if (document.forms["Form"].firstName.value.length == 0) {
         document.getElementById("firstName").style.borderColor = "red";
         alert("Please fill in highlighted text field");
         return false;
     }
-    else if(document.forms["Form"].lastName.value.length ==0)
-    {
+    else if (document.forms["Form"].lastName.value.length == 0) {
         document.getElementById("lastName").style.borderColor = "red";
         alert("Please fill in highlighted text field");
         return false;
     }
-    else if(document.forms["Form"].address1.length ==0)
-    {
+    else if (document.forms["Form"].address1.value.length == 0) {
         document.getElementById("address1").style.borderColor = "red";
         alert("Please fill in highlighted text field");
         return false;
     }
-    else if(document.forms["Form"].address2.length ==0)
-    {
+    else if (document.forms["Form"].address2.value.length == 0) {
         document.getElementById("address2").style.borderColor = "red";
         alert("Please fill in highlighted text field");
         return false;
     }
-    else if(document.forms["Form"].email.length ==0)
-    {
+    else if (document.forms["Form"].email.value.length == 0) {
         document.getElementById("email").style.borderColor = "red";
         alert("Please fill in highlighted text field");
         return false;
     }
-    else if(document.Form.accept.checked == false)
-    {
+    else if (document.Form.accept.checked == false) {
         document.getElementById("Warning").innerHTML = "Please check that you have accepted terms!";
         document.getElementById("Warning").style.color = "red";
         return false;
+    } else {
+        alert("You Did it!")
     }
+
 }
 function change(fieldID){
 
