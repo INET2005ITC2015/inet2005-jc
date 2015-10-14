067cea8dec7d5d89a2cb89907f8eeee647f9249a
jrcooke2 <jrcooke2@gmail.com>
2015-10-14 10:25:15 -0700

    Req 4 - Complete Req 5-9 - In Progress

--- Assignment 1/create.php
+++ Assignment 1/create.php
@@ -6,7 +6,22 @@
 </head>
 <body>
 <?php
+require_once('dbConn.php');
+$db = getDBConnection();
 
+$fName = $_POST["firstName"];
+$lName = $_POST["lastName"];
+$hDate = $_POST["hireDate"];
+$bDate = $_POST["birthDate"];
+$gender = $_POST["male"];
+$gender = $_POST["female"];
+
+    $resultInsert = mysqli_query($db, "INSERT INTO employees (emp_no, first_name, last_name, birth_date, hire_date, gender) VALUES ('$fName','$lName','$bDate','$hDate','$gender')");
+    if (!$resultInsert) {
+        die('Could not insert records to the Employees Database: ' . mysqli_error($db));
+    } else {
+        echo "<h1>Employee Inserted</h1>";
+    }
 ?>
 
 </body>
